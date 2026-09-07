bd4f85d2fd2bfeaf1eebb4789c6d5ea28b8b7305
ManuelDiLena <manuel.dilena.29@gmail.com>
2026-09-07 17:44:28 +0200

    First commit

---
+++ back/src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/health', (req, res) => {
+  res.json({
+    status: 'ok',
+    message: 'E-commerce API is running',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
